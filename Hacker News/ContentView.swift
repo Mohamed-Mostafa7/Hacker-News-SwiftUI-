dba3563920c6f71bc48f57a92d2abfd2e52ffd8a
Mohamed Mostafa <mohameds.mostafas7@gmail.com>
2020-08-23 20:38:28 +0200

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by MoHaMeD MoStAfA on 8/23/20.
+//  Copyright © 2020 MoHaMeD MoStAfA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
